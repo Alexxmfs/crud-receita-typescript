91d5c4e485b2c7a504e3cd6a0980df94a7efdcee
Alex <88356153+Alexxmfs@users.noreply.github.com>
2023-08-02 08:55:15 -0300

    enviando codigo sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE IF NOT EXISTS locluxo DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_0900_ai_ci;
-USE locluxo;
+CREATE DATABASE IF NOT EXISTS receitaDB;
+USE receitaDB;
 
 
-CREATE TABLE veiculo (
-  idveiculo int NOT NULL AUTO_INCREMENT,
-  marca varchar(50) NOT NULL,
-  modelo varchar(50) NOT NULL,
-  ano int NOT NULL,
-  cor varchar(50) NOT NULL,
+CREATE TABLE receita (
+  idreceita int NOT NULL AUTO_INCREMENT,
+  titulo varchar(50) NOT NULL,
+  ingrediente varchar(50) NOT NULL,
+  utensilio varchar(50) NOT NULL,
+  ModoPreparo varchar(50) NOT NULL,
   categoria varchar(50) NOT NULL,
-  PRIMARY KEY (idveiculo)
+  PRIMARY KEY (idreceita)
 );
